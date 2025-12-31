2c8c401b628cafeeec2a90c0745dd6a6b337e8e1
Aashray Jain <aashray.jain@oracle.com>
2025-12-31 14:39:49 +0530

    added access to player attributes

--- player.py
+++ player.py
@@ -5,3 +5,9 @@ class Player:
     def __init__(self, name: str, disc_color: DiscColor):
         self.name = name
         self.disc_color = disc_color
+
+    def get_name(self) -> str:
+        return self.get_name
+
+    def get_disc_color(self) -> str:
+        return self.get_disc_color
