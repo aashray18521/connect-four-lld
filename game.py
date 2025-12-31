780795ddd880df1dc2dd1a8127283cceb858310f
Aashray Jain <aashray.jain@oracle.com>
2025-12-31 14:48:54 +0530

    added dataclass for game class

--- game.py
+++ game.py
@@ -1,17 +1,25 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
 from board import Board
 from player import Player
 from enums.game_state import GameState
 
-from typing import Optional
-
+@dataclass
 class Game:
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-        self.board = Board()
-        self.winner = Optional[Player]
-        self.current_player = self.get_current_player()
-        self.state = self.get_current_game_state()
+    # def __init__(self, player1, player2):
+    #     self.player1 = player1
+    #     self.player2 = player2
+    #     self.board = Board()
+    #     self.winner = Optional[Player]
+    #     self.current_player = self.get_current_player()
+    #     self.state = self.get_current_game_state()
+    player1: Player
+    player2: Player
+    board: Board = field(default_factory=Board)
+    winner: Optional[Player] = None
+    current_player: Optional[Player] = field(init=player1)
+    game_state: GameState = field(init=GameState.IN_PROGRESS)
     
     def get_current_player(self) -> Player:
         if self.current_player is None or self.current_player == self.player2:
