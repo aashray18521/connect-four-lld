0785c3bed59c1884bec3bfa8ad395c38295eef3d
Aashray Jain <aashray.jain@oracle.com>
2025-12-31 14:43:58 +0530

    fixed stupid bug and using dataclass to remove __init__

--- player.py
+++ player.py
@@ -1,13 +1,17 @@
+from dataclasses import dataclass
 from enums.disc_color import DiscColor
 
+@dataclass(frozen=True)
 class Player:
 
-    def __init__(self, name: str, disc_color: DiscColor):
-        self.name = name
-        self.disc_color = disc_color
+    name: str
+    disc_color: DiscColor
+    # def __init__(self, name: str, disc_color: DiscColor):
+    #     self.name = name
+    #     self.disc_color = disc_color
 
     def get_name(self) -> str:
-        return self.get_name
+        return self.name
 
-    def get_disc_color(self) -> str:
-        return self.get_disc_color
+    def get_disc_color(self) -> DiscColor:
+        return self.disc_color
