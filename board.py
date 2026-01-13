90f3c214558f8f162269e5cf2814a3457df2dd52
Aashray Jain <aashray.jain@oracle.com>
2026-01-13 08:54:52 +0530

    defined model classes

--- board.py
+++ board.py
@@ -1,4 +1,35 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from enums.disc_color import DiscColor
+
+@dataclass
 class Board:
-    def __init__(self):
-        self.grid = [["" for _ in range(7)] for _ in range(6)]  # 6 rows x 7 columns
-        
+    rows: int = 6
+    cols: int = 7
+    grid: List[List[Optional[DiscColor]]] = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.grid = [[None for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def get_columns(self) -> int:
+        return self.cols
+    
+    def get_rows(self) -> int:
+        return self.rows
+    
+    def can_place(self, column) -> int:
+        # TODO: Business Logic
+        return True
+    
+    def place_disk(self, player, column) -> int: # row where the disc will fall
+        # TODO: Business Logic
+        return None
+    
+    def check_win(self, player, row, column) -> bool:
+        # TODO: Business Logic
+        return False
+    
+    def is_board_full(self) -> bool:
+        # TODO: Business Logic
+        return False
--- game.py
+++ game.py
@@ -18,8 +18,8 @@ class Game:
     player2: Player
     board: Board = field(default_factory=Board)
     winner: Optional[Player] = None
-    current_player: Optional[Player] = field(init=player1)
-    game_state: GameState = field(init=GameState.IN_PROGRESS)
+    current_player: Optional[Player] = field(init=False)
+    game_state: GameState = field(init=False)
     
     def get_current_player(self) -> Player:
         if self.current_player is None or self.current_player == self.player2:
@@ -39,3 +39,27 @@ class Game:
 
     def get_board(self) -> Board:
         return self.board
+    
+    def make_move(self, player, column) -> bool: ## Can throw exception(s) for invalid moves instead of "False"
+        if(self.game_state != GameState.IN_PROGRESS):
+            return False
+        if(player != self.current_player):
+            return False
+        if(column <= 0 or column >= self.board.get_columns()):
+            return False
+        if(not self.board.can_place(column)):
+            return False
+        
+        row = self.board.place_disk(player=player, column=column)
+
+        if(self.board.check_win(player=player, row=row, column=column)):
+            self.game_state = GameState.WIN
+            self.winner = player
+        elif(self.board.is_board_full()):
+            self.game_state = GameState.DRAW
+        else: # switch turn
+            if(player == self.player1):
+                self.current_player = self.player2
+            else:
+                self.current_player = self.player1
+        return True
